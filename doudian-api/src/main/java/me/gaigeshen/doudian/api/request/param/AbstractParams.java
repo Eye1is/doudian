7d01e1e195861fb45b20fb72554deaa4c27bf548
gaigeshen <gaigeshen@qq.com>
2020-11-05 16:44:15 +0800

    修正业务参数的序列化过程

--- doudian-api/src/main/java/me/gaigeshen/doudian/api/request/param/AbstractParams.java
+++ doudian-api/src/main/java/me/gaigeshen/doudian/api/request/param/AbstractParams.java
@@ -1,12 +1,14 @@
 package me.gaigeshen.doudian.api.request.param;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.SerializableString;
+import com.fasterxml.jackson.core.io.CharacterEscapes;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import me.gaigeshen.doudian.api.util.JsonUtils;
 
 import java.text.SimpleDateFormat;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TreeMap;
 
 /**
@@ -20,17 +22,45 @@ public abstract class AbstractParams extends TreeMap<String, Object> implements 
     objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
     // 只允许包含非空的字段
     objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-    // 特殊字符转义
-    objectMapper.configure(JsonGenerator.Feature.ESCAPE_NON_ASCII, true);
+    // 转义指定的字符
+    objectMapper.getFactory().setCharacterEscapes(new SpecialCharacterEscapes());
   }
 
   @Override
   public final String toJsonString() {
     Map<String, String> stringValueMappings = new TreeMap<>();
     for (Map.Entry<String, Object> entry : entrySet()) {
-      stringValueMappings.put(entry.getKey(), entry.getValue() + "");
+      if (Objects.nonNull(entry.getValue())) {
+        stringValueMappings.put(entry.getKey(), entry.getValue() + "");
+      }
     }
     return JsonUtils.toJson(stringValueMappings, objectMapper);
+  }
+
+  /**
+   * @author gaigeshen
+   */
+  private static class SpecialCharacterEscapes extends CharacterEscapes {
+    private final int[] asciiEscapes;
+
+    public SpecialCharacterEscapes() {
+      int[] esc = CharacterEscapes.standardAsciiEscapesForJSON();
+      esc['<'] = CharacterEscapes.ESCAPE_STANDARD;
+      esc['>'] = CharacterEscapes.ESCAPE_STANDARD;
+      esc['&'] = CharacterEscapes.ESCAPE_STANDARD;
+      esc['\''] = CharacterEscapes.ESCAPE_STANDARD;
+      asciiEscapes = esc;
+    }
+
+    @Override
+    public int[] getEscapeCodesForAscii() {
+      return asciiEscapes;
+    }
+
+    @Override
+    public SerializableString getEscapeSequence(int ch) {
+      return null;
+    }
   }
 
 }
